fdd843d3a730b3e252a38b101cd5bf3ed454475d
wooram <omelisra@naver.com>
2019-05-16 13:04:38 +0900

    3DCN UNET-like

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ from keras.layers import Conv3D, Concatenate
 from keras.layers import LeakyReLU, Deconvolution2D, Deconvolution3D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.pooling import MaxPooling2D, AveragePooling2D, MaxPooling3D, AveragePooling3D
-#from keras.layers.pooling import Upsampling2D, Upsampling3D
+from keras.layers import UpSampling2D, UpSampling3D
 from keras.models import Model
 from keras.layers.core import Lambda, Flatten, Dense
 import math
@@ -37,56 +37,89 @@ def CN3D(video_info=None ,sampling_frame= 32,  vid_net_mid_depth = 3):
     #H = int(H/2)
 
     input_video = Input( shape=(sampling_frame, W, H, 3) )
-
+    print(input_video.get_shape())
     e0 = Conv3D(filters=16,padding='same', kernel_size=(5,5,5))(input_video)
     e0 = Bat(e0)
     e0 = Activ(e0)
+    print(e0.get_shape())
     #WITH NO CONCATENATE FOR ENCODING IN 3DCN BUT WITH CONCAT FOR ENCODING IN combination part
+
     e0_C = Conv3D(filters=32, padding='same', kernel_size=(3,3,3), strides =2)(e0)
     e0_C = Bat(e0_C)
     e0_C = Activ(e0_C)
-    
+    print(e0_C.get_shape())
+
     e1 = Conv3D(filters=64,padding='same', kernel_size=(3,3,3))(e0_C)
     e1 = Bat(e1)
     e1 = Activ(e1)
+    print(e1.get_shape())
 
     e1_C = Conv3D(filters=128, padding='same', kernel_size=3, strides = 2)(e1)
     e1_C = Bat(e1_C)
     e1_C = Activ(e1_C)
-    
-    fc_mid = e1_C
+    print(e1_C.get_shape())
+
+    e2 = Conv3D(filters=256,padding='same', kernel_size=(3,3,3))(e1_C)
+    e2 = Bat(e2)
+    e2 = Activ(e2)
+    print(e2.get_shape())
+
+    e2_C = Conv3D(filters=512, padding='same', kernel_size=3, strides = 2)(e2)
+    e2_C = Bat(e2_C)
+    e2_C = Activ(e2_C)
+    print(e2_C.get_shape())
+
+    fc_mid = e2_C
     p_num = 2
     
     for i in range(vid_net_mid_depth):
-        fc_mid = Conv3D(filters= 256, dilation_rate= p_num, kernel_size = 3, padding='same')(fc_mid)
+        fc_mid = Conv3D(filters= 512, dilation_rate= p_num, kernel_size = 3, padding='same')(fc_mid)
         fc_mid = Bat(fc_mid)
         fc_mid = Activ(fc_mid)
+        print(fc_mid.get_shape())
         p_num = p_num * 2
 
-    fc_mid = Conv3D(strides=1, filters=128, kernel_size= 4, padding='same')(fc_mid)
-    fc_mid = Bat(fc_mid)
-    fc_mid = Activ(fc_mid)
-     
-    fc_mid = Concatenate()([fc_mid, e1_C])
-    fc_mid = Deconvolution3D(strides=2, filters=64, kernel_size= 4, padding='same')(fc_mid)
-    fc_mid = Bat(fc_mid)
-    fc_mid = Activ(fc_mid)
+    #fc_mid = Deconvolution3D(strides=2, filters=64, kernel_size= 4, padding='same')(fc_mid)
+    d0_C = Concatenate()([fc_mid, e2_C])    
+    d0_C = UpSampling3D()(d0_C)
+    d0_C = Conv3D(strides=1, filters=256, kernel_size= 4, padding='same')(d0_C)
+    d0_C = Bat(d0_C)
+    d0_C = Activ(d0_C)
+    print(d0_C.get_shape())
 
-    d1_C = Concatenate()([fc_mid, e1])    
+    d0_CC = Concatenate()([d0_C, e2])    
+    d0_CC = Conv3D(filters=128,padding='same', kernel_size=3)(d0_CC)
+    d0_CC = Bat(d0_CC)
+    d0_CC = Activ(d0_CC)
+    print(d0_CC.get_shape())
+
+    d1 = Concatenate()([d0_CC, e1_C])
+    d1 = UpSampling3D()(d0_CC)
+    d1 = Conv3D(strides=1, filters=64, kernel_size= 4, padding='same')(d1)
+    d1 = Bat(d1)
+    d1 = Activ(d1)
+    print(d1.get_shape())
+
+    d1_C = Concatenate()([d1, e1])    
     d1_C = Conv3D(filters=32,padding='same', kernel_size=3)(d1_C)
     d1_C = Bat(d1_C)
     d1_C = Activ(d1_C)
+    print(d1_C.get_shape())
 
     d1_CC = Concatenate()([d1_C, e0_C])
-    d1_CC = Deconvolution3D(strides=2, filters=16, kernel_size= 4, padding='same')(d1_CC)
+    #d1_CC = Deconvolution3D(strides=2, filters=16, kernel_size= 4, padding='same')(d1_CC)
+    d1_CC = UpSampling3D()(d1_CC)
+    d1_CC = Conv3D(strides=1, filters=16, kernel_size= 4, padding='same')(d1_CC)
     d1_CC = Bat(d1_CC)
     d1_CC = Activ(d1_CC)
+    print(d1_CC.get_shape())
 
     d2_CC = Concatenate()([d1_CC, e0])
     d2_CC = Conv3D(filters=3, padding='same', kernel_size=3)(d2_CC)
     d2_CC = Bat(d2_CC)
     d2_CC = Activation('tanh')(d2_CC)
-    
+    print(d2_CC.get_shape())
+
     video_3DCN = Model(input_video, d2_CC)
 
     return video_3DCN
